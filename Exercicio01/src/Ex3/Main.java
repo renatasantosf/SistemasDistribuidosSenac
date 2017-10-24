aeb1291455feaf4a5eab338e3f7dde162f900cef
Renata Fraga <renatasfraga@gmail.com>
2017-10-24 10:15:05 -0200

    Nova implementacao no ex 2 de threads

--- Exercicio01/src/Ex3/Main.java
+++ Exercicio01/src/Ex3/Main.java
@@ -1,10 +1,19 @@
 package Ex3;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Thread1 t1 = new Thread1();
+        new Thread(t1).start();
+        
+       
+        
+        
+        
+        
+        
 	}
 
 }
--- Exercicio01/src/Ex3/Thread1.java
+++ Exercicio01/src/Ex3/Thread1.java
@@ -3,21 +3,28 @@ package Ex3;
 import java.util.Scanner;
 
 public class Thread1 implements Runnable{
+	
+	private int[] vetor = new int[200];
 
     @Override
     public void run() {
+    	
+    	for(int i=0;i<200;i++) {
+    		vetor[i] = (int) Math.random();
+    	}
+    	
+    	
     	 Scanner ler = new Scanner(System.in);
-    	 System.out.println("Busca 1: Digite o valor que você deseja: ");
+    	 System.out.println("Busca 1: Digite o valor que voce deseja: ");
     	 int busca = ler.nextInt();
-    	 Vetor vetor = new Vetor();
-    	 int cont=0;
-    	 while(cont<=99 && vetor.vet[cont]!=busca) {
-    		 cont++;
-    		 System.out.println(-1);
-    		
-    	 }
+    	
+    	 
+    	for(int i=0;i<100 && vetor[i]!=busca;i++) {
+    		System.out.println(-1);
+    	}
     	 
     	
+    	ler.close();
     	 
     	
             
